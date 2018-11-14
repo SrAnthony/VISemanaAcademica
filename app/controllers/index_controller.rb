7fb650e0af8135f50392c989ab8df459fac580e5
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-11-14 12:59:47 -0200

    Ajuste números exibidos no index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,10 @@
 class IndexController < ApplicationController
-  def index; end
+  def index
+    @users_count = User.count
+    @users_cat_1 = User.where(category: 1).count
+    @users_cat_2 = User.where(category: 2).count
+    @users_cat_3 = User.where(category: 3).count
+  end
 
   def under_construction; end
 end
